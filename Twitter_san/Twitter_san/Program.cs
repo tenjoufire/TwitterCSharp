7e9490f7305068031474190cd706550700708335
tenjoufire <tenjoufire7@gmail.com>
2016-11-04 14:38:44 +0900

    init

---
+++ Twitter_san/Twitter_san/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Twitter_san
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
