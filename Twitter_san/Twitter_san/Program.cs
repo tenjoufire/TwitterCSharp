9746419c8d36e042f1899d90b8f99dee3d1f8fb1
tenjoufire <tenjoufire7@gmail.com>
2016-11-15 14:35:17 +0900

    ignore appconfig

--- Twitter_san/Twitter_san/Program.cs
+++ Twitter_san/Twitter_san/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CoreTweet;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,15 @@ namespace Twitter_san
     {
         static void Main(string[] args)
         {
+            var tokens = Tokens.Create("{API key}"
+                  , "{API secret}"
+                  , "{Access token}"
+                  , "{Access token secret}");
+
+            var text = "status text";
+            tokens.Statuses.Update(new { status = text });
+
+
         }
     }
 }
