1884626573c80220df0a6e3e6197e145947d918b
tenjoufire <tenjoufire7@gmail.com>
2016-11-15 19:16:08 +0900

    とりあえずツイートできた

--- Twitter_san/Twitter_san/Program.cs
+++ Twitter_san/Twitter_san/Program.cs
@@ -1,6 +1,8 @@
 ﻿using CoreTweet;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,13 +13,29 @@ namespace Twitter_san
     {
         static void Main(string[] args)
         {
-            var tokens = Tokens.Create("{API key}"
-                  , "{API secret}"
-                  , "{Access token}"
-                  , "{Access token secret}");
+            // get strings from App.config
+            string APIkey = ConfigurationManager.AppSettings["APIkey"];
+            string APIsecret = ConfigurationManager.AppSettings["APIsecret"];
+            string AccessToken = ConfigurationManager.AppSettings["AccessToken"];
+            string AccessTokenSecret = ConfigurationManager.AppSettings["AccessTokenSecret"];
 
-            var text = "status text";
-            tokens.Statuses.Update(new { status = text });
+            //Console.WriteLine(APIkey + " " + APIsecret + " " + AccessToken + " " + AccessTokenSecret); //for debug
+
+            var tokens = Tokens.Create(APIkey
+                  , APIsecret
+                  , AccessToken
+                  , AccessTokenSecret);
+
+            var text = "なにやってくれてるんだよ";
+            try
+            {
+                tokens.Statuses.Update(new { status = text });
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine("error : " + e.Message);
+            }
+            
 
 
         }
